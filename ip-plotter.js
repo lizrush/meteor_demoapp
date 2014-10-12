c702503950955d5ab8b11bd3f7c4757fa4fee61f
liz rush <liz@lizmrush.com>
2014-10-12 12:48:27 -0700

    Set IPs as arrays

--- ip-plotter.js
+++ ip-plotter.js
@@ -2,8 +2,12 @@ if (Meteor.isClient) {
 
   Template.ipform.events({
     'click button': function () {
-      console.log("form button clicked");
-      // TODO:  input as array
+
+      var input_string = document.getElementById("ip-inputs").value;
+      var ip_array = input_string.replace(/\s+/g, '').split(",");
+
+      console.log(ip_array)
+      // TODO:
       //        iterate over ips
       //        fetch latlongs
       //        drop pins on map
@@ -13,8 +17,19 @@ if (Meteor.isClient) {
   Template.example.events({
     'click button': function () {
       console.log("example button clicked");
-      // TODO:  store lat longs for hard coded ips
+
+      var example_ips = [[51.5, -0.13],
+                          [57.7333, 12.05],
+                          [13.7, 100.4667],
+                          [53.4559, 35.188],
+                          [25, 45],
+                          [42.556, -83.1178],
+                          [40.5, 47.5],
+                          [17.4833, -88.1833],
+                          [-8.05, -34.9]];
+
       //        drop pins on map
+    }
   });
 
   Template.map.events({
