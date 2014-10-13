7c65d8af7fd1434ca6a77f4a9b3dfee0ce3be71d
liz rush <liz@lizmrush.com>
2014-10-13 16:37:55 -0700

    Initialize Mapper

---
+++ map.js
@@ -0,0 +1,18 @@
+var Mapper = {
+
+    map: null,
+    lat_long_bounds: null,
+    baseUrl: "http://freegeoip.net/json/",
+
+    initializeMap: function(mapId){
+        Mapper.lat_long_bounds = new google.maps.LatLngBounds();
+
+        var mapOptions = {
+            zoom: 1,
+            center: new google.maps.LatLng(30, -10),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        Mapper.map = new google.maps.Map(document.getElementById(mapId), mapOptions);
+    }
+}
